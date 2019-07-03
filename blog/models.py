e4cc611ec04b3b30b764491c5ed121f2e17ece6b
Arif Khan <arifkhanesm.com>
2019-07-03 15:53:37 +0530

    This is trial project

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,10 @@ class Blog(models.Model):
     body=models.TextField()
     title=models.CharField(max_length=10000)
     date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+    def summary(self):
+        return self.body[:100]
+    def pub_date(self):
+        return self.date.strftime('%b %e %Y')
